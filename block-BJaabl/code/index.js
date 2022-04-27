8f4797bd0cd95a4c4649494c07509a996b17075c
rcc0404 <rahulchaudhar3@gmail.com>
2022-04-27 10:38:40 +0530

    function

--- block-BJaabl/code/index.js
+++ block-BJaabl/code/index.js
@@ -2,6 +2,13 @@
 1. Create a function named `sayHello` that that accepts a parameter `name` and alert `Hello [name]!`.
 */
 
+// function sayHello() {
+//   let name = prompt("Enter name");
+//   alert(`Hello ${name}`);
+// }
+
+// sayHello();
+
 /*
 2. Create a function named `getFullName` that accepts two parameter `firstName` and `lastName` and returns
 fullName.
@@ -11,6 +18,13 @@ Example:
   getFullName("Nelson", "Mandela"); // "Nelson Mandela"
 */
 
+// function getFullName() {
+//   let firstName = prompt("Enter First Name?");
+//   let secondName = prompt("Enter second name?");
+//   alert(`${firstName} ${secondName}`);
+// }
+
+// getFullName();
 /*
 3. Create a function named `addTwoNumbers` that accepts two numbers i.e `firstNum` and `secondNum` and returns
 the sum of both numbers. If user passes any data other then numbers alert a message saying `Enter Valid Input`
@@ -20,6 +34,15 @@ addTwoNumbers(10, 22); // 32
 addTwoNumbers(20, 32); // 32
 addTwoNumbers(10, "100"); // Alert Enter Valid Input
 */
+// function addTwoNumbers() {
+//   let firstNum = +prompt("Enter first number?");
+//   let secondNum = +prompt("Enter second number?");
+//   addTwoNumbers = firstNum + secondNum;
+//   alert(addTwoNumbers);
+// }
+
+// addTwoNumbers();
+
 
 /*
 4. Create a function named `calc` which accepts three parameter `numA`, `numB` and `operation`. Operation
@@ -33,6 +56,12 @@ calc(20, 10, 'sub'); // 10
 calc(20, 10, 'mul'); // 200
 */
 
+function calc() { 
+  let numA = +prompt("Enter first number");
+  let numB = +prompt("Enter second number");
+  let operation = (add)
+
+}
 
 /*
 5. Create a function named `isLeapYear` that accepts a number data type and return `true` or `false` based
@@ -42,7 +71,15 @@ isLeapYear(2000); // true
 isLeapYear(2001); // false
 */
 
+function isLeapYear() {
+  let 
+}
+
 
 /*
 6. Create a function named `getFactorial` that accepts a number and return the factorial of the number.
 */
+function getFactorial() {
+  
+}
+  
