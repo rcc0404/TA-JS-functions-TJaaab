844a2a54c62a5d7e8435eac1443f04ed05284a2b
rcc0404 <rahulchaudhar3@gmail.com>
2022-04-29 09:17:55 +0530

    asignment no 2

--- block-BJaabv/code/index.js
+++ block-BJaabv/code/index.js
@@ -3,15 +3,16 @@
 /*
 1. 🎖Write a function named calculateDogAge that:
   * [ ] Takes 1 argument: your puppy's age.
-  * [ ] Calculates your dog's age based on the conversion rate of 1 human year to 7 dog years.
+  * [ ] Calculates your dog's age based on the  conversion rate of 1 human year to 7 dog years.
   * [ ] Return the result dog age in dog year.
   * [ ] Add an additional argument to the function that takes the conversion rate of human to dog years.
 */
 
-function calculateDogAge() {
-  // Your code goes here 
-  if
-}
+// function calculateDogAge(puppyAge, conversion = 7) {
+//   return puppyAge * conversion;
+// }
+
+// console.log(calculateDogAge(15, 3));
 
 /*
 2. 🎖Write a function named calculateMoviesToWatch that:
@@ -20,9 +21,14 @@ function calculateDogAge() {
   * [ ] Return the total number of movie you will watch.
 */
 
-function calculateMoviesToWatch() {
-  // Your code goes here
-}
+// function calculateMoviesToWatch(age, noOfMovies) {
+//   const MAX_AGE = 80;
+//   let totalNumberOfMovies = (MAX_AGE - age) * 12 * 4 * noOfMovies;
+
+//   return totalNumberOfMovies;
+// }
+
+// console.log(calculateMoviesToWatch(70, 4));
 
 /*
 3. 🎖Create a function called celsiusToFahrenheit:
@@ -30,8 +36,9 @@ function calculateMoviesToWatch() {
   * [ ] Convert it to fahrenheit and return "NN°F is NN°C"
 */
 
-function celsiusToFahrenheit() {
-  // Your code goes here
+function celsiusToFahrenheit(temp) {
+  let fahrenheit = (temp * 9) / 5 + 32;
+  return `${temp}°F is ${fahrenheit}°C`;  
 }
 
 /*
@@ -40,8 +47,9 @@ function celsiusToFahrenheit() {
   * [ ] Convert it to celsius and return "NN°F is NN°C"
 */
 
-function celsiusToFahrenheit() {
-  // Your code goes here
+function fahrenheitToCelsius(temp) {
+  let celsius = (temp - 32) * 5 / 9;
+  return `${temp}°F is ${celsius}°C`;
 }
 
 /*
@@ -52,8 +60,12 @@ function celsiusToFahrenheit() {
   * [ ] If the value of n is below 0 return "The number below 1 is not allowed"
 */
 
-function pow() {
-  // Your code goes here
+function pow(x, n) {
+  if (n < 0) {
+    return "The nuber below 1 is not allowed";
+  } else {
+    return x ** n;
+  }
 }
 
 // Test
@@ -67,8 +79,22 @@ pow(-31, 2); // "The number below 1 is not allowed"
 and return sum or product of 1,…,n. If user enters any other value than `sum` or `product` alert `Not a valid Input`.
 */
 
-function sumOrProductOfN() {
-  // Your code goes here
+function sumOrProductOfN(num, operation) {
+  if (operation === "sum") {
+    let sum = 0;
+    for (let i = 1; i <= num; i++) {
+      sum += i;
+    }
+    return sum;
+  } else if (operation === "product") {
+    let product = 1;
+    for (let i = 1; i <= num; i++) {
+      product *= i;
+    }
+    return product;
+  } else {
+    alert("Not a valid input");
+  }
 }
 
 sumOrProductOfN(4, 'sum'); // 10
@@ -79,22 +105,34 @@ sumOrProductOfN(4, 'hello'); // alert "Not a valid Input"
 6. 🎖Write a program that accepts a number n using argument and return the sum of the numbers 1 to n
 */
 
-function sumOfN() {
-  // Your code goes here
+function sumOfN(n) {
+    let sum = 0;
+    for (let i = 1; i <= n; i++) {
+      sum += i;
+    }
+    return sum;
 }
 
 /*
 7. 🎖Modify the previous program such that only multiples of 5 or 7 are considered in the sum, e.g. n = 20 (5,7,10,14,15,20) 71
 */
-
-// Your code goes here
+function sumOfNWithFive(n) {
+  let sum = 0;
+  for (let i = 1; i <= n; i++) {
+    if (i % 5 === 0 || 1 % 7 === 0) { 
+    sum += i;
+  }
+}
+    return sum;
+}
 
 /*
 8. 🎖Write a function `min` that takes two arguments and returns their minimum.
 */
 
-function min() {
-  // Your code goes here
+function min(a, b) {
+  if (a < b) return a;
+  if (b < a) return b; 
 }
 
 min(0, 10);
@@ -104,6 +142,6 @@ min(0, -10);
 9. 🎖Write a JavaScript function which accepts an argument and returns the type of the value.
 */
 
-function typeCheck() {
-  // Your code goes here
+function typeCheck(val) {
+  return typeof val;
 }
